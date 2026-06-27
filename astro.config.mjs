da9472b0de49492ff345973143c33582778c7d3f
RaghuKolipakula <kolipakula@gmail.com>
2026-06-27 16:37:41 -0500

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://anythingfromamerica.com',
-  integrations: [sitemap()],
 });
